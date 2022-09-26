e409f295360606658b12af91898a9d816eb98926
DispicableLee <lee.robert053@gmail.com>
2022-09-26 12:50:56 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Add your code here
+const configObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify({
+        dogName: "Byron",
+        dogBreed: "Poodle",
+    })
+}
+
+fetch("http://localhost:3000/dogs", configObject)
+    .then(function(res){
+        return res.json();
+    })
+    .then(function (object){
+        console.log(object);
+    })
+    .catch((err)=>{
+        alert("bad things! Ragnarok!");
+        console.log(err.message);
+    })
+
+
+
+function submitData(uName, uEmail){
+
+    const formData = {};
+    formData.userName = uName;
+    formData.userEmail = uEmail;
+    
+    const configObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData)
+    }
+
+
+    fetch("http://localhost:3000/users", configObject)
+    .then((res)=>{
+        return res.json();
+    })
+    .then((object)=>{
+        console.log(object)
+    })
+    .catch((err)=>{
+        alert(err.message)
+    })
+
+}
+
+submitData("ROb", "lee.robert053@gmail.com")
